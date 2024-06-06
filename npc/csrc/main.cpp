e819a71daec2e86e69ad5ce33f1790138f4f082b
rache <2746324475@qq.com>
2024-06-06 22:43:34 +0800

    verilog learning...

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include "verilated.h"
+#include "../obj_dir/Vexample.h"
 
 int main() {
-  printf("Hello, ysyx!\n");
+  Vexample *example = new Vexample;
+  for (int a = 0; a < 2; a++)
+  {
+    for (int b = 0; b < 2; b++) {
+      example->a = a;
+      example->b = b;
+      example->eval();
+      printf("a=%d, b=%d, f=%d, correct=%d\n", a, b, example->f, a^b);
+    }
+  }
   return 0;
 }
