81be37d9192fefcf9d0a0ccbf799ca9ce55c3783
tracer-ysyx <tracer@ysyx.org>
2024-08-30 16:25:13 +0800

    > sim RTL ysyx_24080016 刘镇睿 Linux racheMint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:25:13 up 21:44,  1 user,  load average: 0.86, 0.66, 0.57

--- npc/csrc/util/cachesim/src/cache.cpp
+++ npc/csrc/util/cachesim/src/cache.cpp
@@ -46,6 +46,7 @@ SimResult Cache::sim(Tracer &tracer) {
                 result.writeMiss++;
             }
         }
+        std::cout << std::hex << addr << std::endl;
     }
     return result;
 }
