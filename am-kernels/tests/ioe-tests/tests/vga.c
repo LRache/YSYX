43cd02d162a465e1fbdbc17902c4f0a1bcda3dd1
tracer-ysyx <tracer@ysyx.org>
2024-08-31 16:18:59 +0800

    > compile RTL ysyx_24080016 刘镇睿 Linux racheMint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:18:59 up 1 day, 21:38,  1 user,  load average: 1.46, 1.08, 1.02

--- am-kernels/tests/ioe-tests/tests/vga.c
+++ am-kernels/tests/ioe-tests/tests/vga.c
@@ -1,27 +1,32 @@
 #include "am.h"
+#include "klib.h"
 
 #define VGA_BASE 0x21000000
-#define VGA_HEIGHT 480
-#define VGA_WIDTH 640
+#define VGA_HEIGHT 100
+#define VGA_WIDTH 100
 
 int main() {
     // uint32_t buffer[10000];
     // for (int i = 0 ;i < 10000; i++) {
     //     buffer[i] = 0xffffff;
     // }
-    uint32_t color = 0;
+    uint32_t buffer[VGA_WIDTH][VGA_HEIGHT];
+    // uint32_t color = 0;
     for (int y = 0; y < VGA_HEIGHT; y++) {
         for (int x = 0; x < VGA_WIDTH; x++) {
-            AM_GPU_FBDRAW_T ctl;
-            ctl.h = 1;
-            ctl.w = 1;
-            ctl.x = x;
-            ctl.y = y;
-            ctl.pixels = &color;
-            ioe_write(AM_GPU_FBDRAW, &ctl);
-            color = (color + 1) % 0x00ffffff;
+            buffer[x][y] = 0x12;
+            // color += 32;
         }
+        printf("%d\n", y);
     }
+    puts("start\n");
+    AM_GPU_FBDRAW_T ctl;
+    ctl.h = VGA_HEIGHT;
+    ctl.w = VGA_WIDTH;
+    ctl.x = 0;
+    ctl.y = 0;
+    ctl.pixels = buffer;
+    ioe_write(AM_GPU_FBDRAW, &ctl);
     while (1);    
     return 0;
 }
