dd4698f39690cee1797520e1185c25d0f7b65efa
tracer-ysyx <tracer@ysyx.org>
2024-09-02 09:37:48 +0800

    > generate verilog ysyx_24080016 刘镇睿 Linux racheMint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:37:48 up 3 days, 14:57,  1 user,  load average: 0.92, 0.54, 0.64

--- npc/csrc/src/hdb.cpp
+++ npc/csrc/src/hdb.cpp
@@ -20,7 +20,7 @@ VTop top;
 static uint64_t timer = 0;
 std::string hdb::outputDir = "./";
 
-#define IMG_NAME test_img_dead_loop
+#define IMG_NAME test_img_ebreak
 
 static uint32_t *img = IMG_NAME;
 static size_t img_size = sizeof(IMG_NAME);
