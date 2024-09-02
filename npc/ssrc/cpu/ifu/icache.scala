48fc5162cc679bfa34b9dd4e6f1fc554cabf9557
tracer-ysyx <tracer@ysyx.org>
2024-09-02 08:59:31 +0800

    > generate verilog ysyx_24080016 刘镇睿 Linux racheMint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:59:31 up 3 days, 14:19,  1 user,  load average: 0.79, 1.03, 1.00

--- npc/ssrc/cpu/ifu/icache.scala
+++ npc/ssrc/cpu/ifu/icache.scala
@@ -77,8 +77,11 @@ class ICache (e: Int, s: Int) extends Module {
             group(i)
         )
     }
-    when(memValid) {
-        printf("%x %x %x %x", rdata0, rdata1, rdata2, io.mem.rdata)
+    // when(memValid) {
+    //     printf("%x %x %x %x", rdata0, rdata1, rdata2, io.mem.rdata)
+    // }
+    when(ready) {
+        printf("%d\n", offset)
     }
 
     val memRData = MuxLookup(offset, 0.U)(Seq (
