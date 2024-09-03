b57c5566a36357fac519d44a6700a5445b201631
tracer-ysyx <tracer@ysyx.org>
2024-09-03 10:40:01 +0800

    > sim RTL ysyx_24080016 刘镇睿 Linux racheMint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:40:01 up 4 min,  1 user,  load average: 1.93, 1.35, 0.60

--- npc/csrc/src/hdb.cpp
+++ npc/csrc/src/hdb.cpp
@@ -122,7 +122,7 @@ void hdb_set_csr(uint32_t addr, word_t data) {
         case 6: cpu.mscratch= data; break;
         case 7: cpu.mepc    = data; break;
         case 8: cpu.mcause  = data; break;
-        default: panic("Invalid CSR: %d at pc=0x%08x(inst=0x%08x)", addr, addr, cpu.pc, cpu.inst);
+        default: panic("Invalid CSR: %d at pc=0x%08x(inst=0x%08x)", addr, cpu.pc, cpu.inst);
     }
 }
 
