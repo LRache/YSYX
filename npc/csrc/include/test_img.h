e829030b97affe19b900e34318f7679aac217f8d
tracer-ysyx <tracer@ysyx.org>
2024-09-10 20:03:08 +0800

    > sim RTL ysyx_24080016 刘镇睿 Linux racheMint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:03:08 up 18:01,  1 user,  load average: 0.32, 0.67, 0.85

--- npc/csrc/include/test_img.h
+++ npc/csrc/include/test_img.h
@@ -405,7 +405,8 @@ static uint32_t test_img_control_hazard3[] = {
 
 static uint32_t test_img_control_hazard4[] = {
     0x00000097, // 00 auipc x1, 0
-    0x00008067, // 04 jalr x0, 12(x1)
+    0x01008093, // 04 addi x1, x1, 16
+    0x00008067, // 04 jalr x0, 0(x1)
     0x00000000, // 08 invalid
     GOOD_TRAP
 };
