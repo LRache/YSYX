8d31f55d20cb06f12ab2ffece7ad14172d0cbeea
tracer-ysyx <tracer@ysyx.org>
2024-09-10 19:27:28 +0800

    > sim RTL ysyx_24080016 刘镇睿 Linux racheMint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:27:28 up 17:25,  1 user,  load average: 0.90, 0.70, 0.58

--- npc/csrc/src/hdb.cpp
+++ npc/csrc/src/hdb.cpp
@@ -21,7 +21,7 @@ VTop top;
 static uint64_t timer = 0;
 std::string hdb::outputDir = "./";
 
-#define IMG_NAME test_img_control_hazard2
+#define IMG_NAME test_img_branch
 static uint32_t *img = IMG_NAME;
 static size_t img_size = sizeof(IMG_NAME);
 
