0676be1213ef0c6794917911c6eb156ad0743b21
tracer-ysyx <tracer@ysyx.org>
2024-09-11 13:01:12 +0800

    > compile NEMU ysyx_24080016 刘镇睿 Linux racheMint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:01:12 up 1 day, 10:59,  1 user,  load average: 1.01, 0.88, 0.77

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -93,20 +93,21 @@ void exec_once(Decode *s, vaddr_t pc) {
 }
 
 void execute(uint64_t n) {
-  Decode s;
-  for (;n > 0; n --) {
-    exec_once(&s, cpu.pc);
-    g_nr_guest_inst ++;
-    trace_and_difftest(&s, cpu.pc);
-    if (nemu_state.state != NEMU_RUNNING) break;
-    IFDEF(CONFIG_DEVICE, device_update());
-    // word_t intr = isa_query_intr();
-    // if (intr != INTR_EMPTY) {
-    //   word_t dnpc = isa_raise_intr(intr, cpu.pc-4);
-    //   cpu.pc = dnpc;
-    // }
-    Log("Exec to " FMT_WORD, cpu.pc);
-  }
+    Log("excute");
+    Decode s;
+    for (;n > 0; n --) {
+        exec_once(&s, cpu.pc);
+        g_nr_guest_inst ++;
+        trace_and_difftest(&s, cpu.pc);
+        if (nemu_state.state != NEMU_RUNNING) break;
+        IFDEF(CONFIG_DEVICE, device_update());
+        // word_t intr = isa_query_intr();
+        // if (intr != INTR_EMPTY) {
+        //   word_t dnpc = isa_raise_intr(intr, cpu.pc-4);
+        //   cpu.pc = dnpc;
+        // }
+        Log("Exec to " FMT_WORD, cpu.pc);
+    }
 }
 
 static void statistic() {
