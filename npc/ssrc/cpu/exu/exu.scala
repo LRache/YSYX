48ecf1fa1e47f36840c117e63ea177fd59b48c17
tracer-ysyx <tracer@ysyx.org>
2024-09-12 19:32:30 +0800

    > generate verilog ysyx_24080016 刘镇睿 Linux racheMint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:32:30 up  3:46,  1 user,  load average: 0.40, 0.54, 0.63

--- npc/ssrc/cpu/exu/exu.scala
+++ npc/ssrc/cpu/exu/exu.scala
@@ -47,6 +47,9 @@ class EXU extends Module {
     io.out.bits.exu_result := alu_result
     val jmp = (io.in.bits.is_branch && alu.io.cmp) || io.in.bits.is_jmp
     io.jmp := jmp
+    when (io.in.valid) {
+        printf("0x%x %d\n", io.in.bits.dbg.pc, jmp)
+    }
 
     // CSR
     io.csr_waddr := io.in.bits.csr_waddr
