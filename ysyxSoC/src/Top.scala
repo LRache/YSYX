cbd600cfe46ad62aff9e0b99145a49f122f361ca
rache <2746324475@qq.com>
2024-10-08 21:22:18 +0800

    add delay config

--- ysyxSoC/src/Top.scala
+++ ysyxSoC/src/Top.scala
@@ -8,6 +8,7 @@ import freechips.rocketchip.diplomacy.LazyModule
 object Config {
   def hasChipLink: Boolean = false
   def sdramUseAXI: Boolean = true
+  def hasDelay: Boolean = false
 }
 
 class ysyxSoCTop extends Module {
