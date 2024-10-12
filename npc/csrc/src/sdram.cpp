f83f762c2ba6a71043c73093735f5ec2196ec7a4
rache <2746324475@qq.com>
2024-10-12 13:38:23 +0800

    add debug information

--- npc/csrc/src/sdram.cpp
+++ npc/csrc/src/sdram.cpp
@@ -1,15 +1,15 @@
 #include <stdint.h>
 #include "config.h"
 #include "memory.h"
+#include "debug.h"
 
 #ifdef HAS_SDRAM
 
 uint16_t sdram[4][8192][512];
 
 extern "C" void sdram_read (int bank, int row, int column, uint16_t *data) {
-    // Log("SDRAM Read [%d][%d][%d]=" FMT_WORD, bank, row, column, sdram[bank][row][column]);
-    // assert(0);
     *data = sdram[bank][row][column];
+    // Log("SDRAM Read [%d][%d][%d]=0x%04x" , bank, row, column, *data);
 }
 
 extern "C" void sdram_write(int bank, int row, int column, uint16_t data, uint8_t _mask) {
